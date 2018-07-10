43e46abe5be4ad86e75c291dbc934134eebbd26b
BrisiaCastrejon <brissmonse@gmail.com>
2018-07-10 11:49:32 -0500

    cambios en funciones y DOM

--- src/data.js
+++ src/data.js
@@ -1,57 +1,151 @@
-//let content = document.querySelector("#content");
+// variable para la liga de la data en gitHub
 let url = 'https://raw.githubusercontent.com/BrisiaCastrejon/cdmx-2018-06-bc-core-am-data-dashboard/master/data/laboratoria.json';
-const getAlumnas = () =>{
+// vaariables para impresion
+let contenido = document.getElementById('contenido');
+let screens = document.getElementById('screens');
+let talk = ' ';
+// funcion para cargar la data desde un inicio
+window.onload = () => {
+  // metodo FETCH para la obtención de la data
   fetch(url)
-  .then(response => response.json())
-  .then( data => {
-    let convertData = Object.keys(data);
-       for(sedes in data){
-           let generaciones = Object.keys(data[sedes].generacion);
-         generaciones.forEach((generacion)=> {
-           let selectgeneracion = generacion;
-           let arregloEstudiantes = data[sedes].generacion[generacion].estudiantes;
-         let generaciones = Object.keys(data[sedes].generacion);
-         generaciones.forEach((generacion)=> {
-           let selectgeneracion = generacion;
-           console.log(selectgeneracion);
-         let arregloEstudiantes = data[sedes].generacion[generacion].estudiantes;
-         arregloEstudiantes.forEach((estudiante)=>{
-           let nombre = estudiante.nombre;
-           let correo = estudiante.correo;
-           let turno = estudiante.turno;
-           let progreso = estudiante.progreso.duracionPrograma;
-           let duracion = estudiante.progreso.porcentajeCompletado;
-         let temas = Object.keys(estudiante.progreso.temas);
-         temas.forEach((tema)=>{
-           let selectTema= tema;
-         let arregloTemas= estudiante.progreso.temas[tema];
-           let duracionTema = estudiante.progreso.temas[tema].duracionTema;
-           let duracionTemaCompletado = estudiante.progreso.temas[tema].duracionTemaCompletado;
-           let porcentajeCompletado = estudiante.progreso.temas[tema].porcentajeCompletado;
-           let subtemasCompletados = estudiante.progreso.temas[tema].subtemasCompletados;
-           let subtemasTotales = estudiante.progreso.temas[tema].subtemasTotales;
-        let subtemas = Object.keys(estudiante.progreso.temas[tema].subtemas);
-        subtemas.forEach((subtema)=>{
-          let selectSubtema = subtema;
-        let arregloSubTemas= estudiante.progreso.temas[tema].subtemas[subtema];
-          let completado = estudiante.progreso.temas[tema].subtemas[subtema].completado;
-          let duracionSubtema = estudiante.progreso.temas[tema].subtemas[subtema].duracionSubtema;
-          let puntuacion= estudiante.progreso.temas[tema].subtemas[subtema].puntuacion;
-          let tipo= estudiante.progreso.temas[tema].subtemas[subtema].tipo;
-        })
-       })
-          const lista = document.createElement("li");
-          const contenido = document.createTextNode(sedes +" " + selectgeneracion + " " + nombre + " " + duracion + "% completado");
-          lista.appendChild(contenido);
-          document.body.appendChild(lista);
-
-           const lista = document.createElement("li");
-           const contenido = document.createTextNode(sedes +" " + selectgeneracion + " " + nombre + " " + correo +" "+ turno );
-           lista.appendChild(contenido);
-           document.body.appendChild(lista);
-
+  // se asigna la obtencion en formato JSON
+    .then(data => data.json())
+    // se le asigna nombre a la data
+    .then(laboratoria => {
+      // se verifica que si aparezca en consola
+      // se llaman las variables a ocupar con ese parametro
+      computeStudentsStats(laboratoria);
+      computeGenerationsStats(laboratoria);
     })
-   })
+    .catch(error => {
+      // console.log('Este es un mensaje para reportar error');
+    });
+};
+// variables para sacar la nueva data
+const student = [];
+let arregloEstudiantes = [];
+let name = '';
+let email = '';
+let students = {};
+let completedPercentage;
+let completedPercentageT;
+let topics = {};
+let percentageDuration;
+let subtopics = {};
+let type = '';
+let duration;
+const computeStudentsStats = (laboratoria) => {
+  // se itera el objeto para sacar campus
+  for (sedes in laboratoria) {
+    let campus = sedes;
+    // se itera el objeto para sacar generaciones
+    for (generaciones in laboratoria[sedes]) {
+      let posicionGeneracion = generaciones;
+      // se itera el objeto para sacar generacion
+      for (generacion in laboratoria[sedes][generaciones]) {
+        let generation = generacion;
+        // se llama el arreglo para declarar la ruta hasta estudiantes
+        arregloEstudiantes = laboratoria[campus].generacion[generacion].estudiantes;
+        arregloEstudiantes.forEach((estudiante)=>{
+          name = estudiante.nombre;
+          email = estudiante.correo;
+          completedPercentage = estudiante.progreso.porcentajeCompletado;
+          let prueba = estudiante.progreso.temas;
+          for (item in prueba) {
+            topics = item;
+            completedPercentageT = estudiante.progreso.temas[item].porcentajeCompletado;
+            percentageDuration = estudiante.progreso.temas[item].duracionTemaCompletado;
+            let prueba2 = estudiante.progreso.temas[item].subtemas;
+            for (elementos in prueba2) {
+              subtopics = elementos;
+              type = estudiante.progreso.temas[item].subtemas[elementos].tipo;
+              duration = estudiante.progreso.temas[item].subtemas[elementos].duracionSubtema;
+            }
+          }
+          student.push({
+            'name': name,
+            'email': email,
+            'campus': campus,
+            'generation': generation,
+            stats: {
+              'completedPercentage': completedPercentage,
+              topics: {
+                'completePercentage': completedPercentageT,
+                'percentageDuration': percentageDuration,
+                subtopics: {
+                  'type': type,
+                  'duration': duration
+                }
+              }
+            }
+          });
+        });
+      }
+    }
   }
- })
-}
+  impresionStudents(student);
+};
+const impresionStudents = (student) => {
+  document.getElementById('pruebas').addEventListener('click', (event) => {
+    let result = '';
+    const info = Object.values(student);
+    for (let i = 0; i < info.length; i++) {
+      screens.style.display = 'none';
+      result += `<div class="card text-center float-left pt-2" style="width: 18rem;">
+         <i class="far fa-grin card-img-top smile"></i>
+         <div class="card-body txto">
+          <p>
+         <b class="boldi">${info[i].name}</b> <br>
+         ${info[i].email} <br>
+         Sede: ${info[i].campus} <br>
+         Generación: ${info[i].generation} <br>
+         </p>
+         </div>
+          </div> `;
+    } contenido.innerHTML = result;
+  });
+};
+
+// variables para sacar el arreglo con propiedades: porcentaje, sede, numero de estudiantes y generacion
+let generation = [];
+let generacion = '';
+let campus = '';
+let average = 0;
+let count = 0;
+const computeGenerationsStats = (laboratoria) => {
+  // se itera el objeto para sacar campus
+  for (items in laboratoria) {
+    // se declara una variable que guarde el campus
+    campus = items;
+    // convierte generacion en array para poder iterarla
+    generacion = Object.keys(laboratoria[campus].generacion);
+    generacion.forEach(item => {
+      // se declara el contador para guardar la data
+      let cont = 0;
+      // se obtiene cada generacion
+      generacion = item;
+      // se declara una variable para ruta del objeto hasta estudiantes
+      const chicas = laboratoria[campus].generacion[generacion].estudiantes;
+      // se itera el objeto para sacar de cada chica el porcentajeCompletado
+      for (chica in chicas) {
+        // se declara una variable que guarde a las estudiante, si se declara fuera de la funcion itera 2 veces
+        let estudiantes = chica;
+        // se declara el contador para sumar la data
+        cont += chicas[estudiantes].progreso.porcentajeCompletado;
+        // se utiliza Math.roud para redondear la cifra final, y se hace la operacion
+        average = Math.round(cont / chicas.length);
+        //  se declara una variable para obtener el numero de chicas que estan poro generacion
+        count = chicas.length;
+      }
+      // se manda la info al array con llaves para indicar que es objeto
+      generation.push({
+        'generation': generacion,
+        'campus': campus,
+        'average': average,
+        'count': count
+      });
+    });
+  }
+  // console.log(generation);
+  return generation;
+};
--- src/main.js
+++ src/main.js
@@ -1,27 +1,18 @@
-// ----------------   Login   -------------------------
-
-// Trae elementos del HTML para la pantalla de Login
-let loginScreen = document.getElementById ('loginScreen');
-let loginBtn = document.getElementById('loginBtn');
+document.getElementById('loginBtn').addEventListener('click', (event) => {
+  if (user.value === 'Laboratoria' && password.value === '1234') {
+    window.location.href = 'data.html';
+  } else {
+    loginError.innerHTML = '<p>El usuario o contraseña son incorrectos</p><p>Usuario: Laboratoria</p> <p>Contraseña: 1234 </p>';
+  }
+});
+let loginScreen = document.getElementById('loginScreen');
 let user = document.getElementById('user');
 let password = document.getElementById('password');
 let prueba = document.getElementById('prueba');
 let loginError = document.getElementById('loginError');
 
-// Botón Login
-//loginBtn.addEventListener('click', login);
-
-// ---------------------   Acaba Login   ---------------------
-
-
-
-// ---------------------  Menú    ----------------------------
 let general = document.getElementById('general');
 let lima = document.getElementById('lima');
 let mexico = document.getElementById('mexico');
 let santiago = document.getElementById('santiago');
 let alumnas = document.getElementById('alumnas');
-
-
-//general.addEventListener('click', getData);
-alumnas.addEventListener('click', getAlumnas);
